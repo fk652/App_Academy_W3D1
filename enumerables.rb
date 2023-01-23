e0d18347d8c759972077d5c586b77db0c5ae2a63
Fahim Khan <fk652@nyu.edu>
2023-01-23 17:07:30 -0500

    zip rotate

--- enumerables.rb
+++ enumerables.rb
@@ -50,7 +50,31 @@ class Array
     end
     new_arr
   end
+
+  def my_zip(*args)
+    new_arr = []
+    (0..self.length-1).each do |index|
+      sub_arr = []
+      sub_arr << self[index]
+      args.each do |arr|
+        sub_arr << arr[index]
+      end
+      new_arr << sub_arr
+    end
+    new_arr
+  end
   
+  def my_rotate(num=1)
+    new_arr = self.dup
+    if num > 0
+      num.times { new_arr.push(new_arr.shift) }
+    else
+      (num * -1).times { new_arr.unshift(new_arr.pop) }
+    end
+    new_arr
+  end
+
+  def 
 end
 
 # return_value = [1, 2, 3].my_each do |num|
@@ -61,7 +85,7 @@ end
 
 # p return_value
 
-a = [1, 2, 3]
+# a = [1, 2, 3]
 # p a.my_select { |num| num > 1 } # => [2, 3]
 # p a.my_select { |num| num == 4 } # => []
 
@@ -73,4 +97,20 @@ a = [1, 2, 3]
 # p a.my_all? { |num| num > 1 } # => false
 # p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
