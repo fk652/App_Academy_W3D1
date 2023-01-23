bc4c16993deebdf7c768b890bdbd462fe3340705
Fahim Khan <fk652@nyu.edu>
2023-01-23 16:35:55 -0500

    my_each and my_select

---
+++ enumerables.rb
@@ -0,0 +1,31 @@
+class Array
+  def my_each(&prc)
+    i = 0
+    while i < self.length
+      prc.call(self[i])
+      i += 1
+    end
+    self
+  end
+
+  def my_select(&prc)
+    new_arr = []
+    self.my_each do |elem|
+      new_arr << elem if prc.call(elem)
+    end
+    new_arr
+  end
+  
+end
+
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+
+# p return_value
+
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
