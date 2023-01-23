7d225401a18ec1f71717bc1bdde897b6b5aaaad6
Fahim Khan <fk652@nyu.edu>
2023-01-23 17:52:23 -0500

    fixed merges

--- enumerables.rb
+++ enumerables.rb
@@ -129,9 +129,6 @@ p [7].my_flatten
 # p a.my_rotate         #=> ["b", "c", "d", "a"]
 # p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
 # p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
-<<<<<<< HEAD
-# p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
-=======
 # p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
 
 # a = [ "a", "b", "c", "d" ]
@@ -140,4 +137,3 @@ p [7].my_flatten
 
 # p [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
 # p [ 1 ].my_reverse               #=> [1]
->>>>>>> 76b9f8330a6deb2efe09071efa54819860cd6240
